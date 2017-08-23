46ed2eea3dca3a45de9baa91299848ad55e32a33
annacrespo12 <annacrespo12@gmail.com>
2017-08-22 19:23:28 -0700

    buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+//Click start
+// Timer ticks down 120 seconds
+// When timer hits zero trivia all done
+// Correct answers
+// Incorrect answers
+// Unanswered
+// Only one answer per question
+
+$(document).ready(function() {
+	function initialScreen() {
+		startScreen()
+	}
+}
+
+var questionArray = {
+  q1: 'What?',
+  answers: ["work", "touched tree", "pizza", "running"],
+
+
+};
+
+
+<label>
+        <input type="radio" name="answers[2]" value="B" data-correct>
+        Answer B
+      </label>
