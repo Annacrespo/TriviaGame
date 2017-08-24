35c0cbb6cd990de4dabb0d67e65384e95ffc36db
annacrespo12 <annacrespo12@gmail.com>
2017-08-23 22:19:44 -0700

    questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,9 +4,12 @@
 // Correct answers
 // Incorrect answers
 // Unanswered
-// Only one answer per question
+
 var number = 60;
 var intervalId;
+var wins = 0;
+var losses = 0;
+var incomplete = 0;
 
 
 // $(document).ready(function() {
@@ -18,65 +21,78 @@ var intervalId;
 
 //jumbotron toggle when start is pressed
 
-$('#jumbo').on('click', function(){
-	$('#jumbo').toggle();
+$('#jumbo').on('click', function() {
+    $('#jumbo').toggle();
+    $('#questionOneContainer').show();
+    
 
-});
-
-
-// timer
-$("#btn btn-primary btn-lg").on("click", run);
-function run() {
-      intervalId = setInterval(decrement, 1000);
+    function run() {
+        intervalId = setInterval(decrement, 1000);
+        decrement.start();
     }
 
     //  The decrement function.
     function decrement() {
 
-      //  Decrease number by one.
-      number--;
+        //  Decrease number by one.
+        number--;
 
-      //  Show the number in the #show-number tag.
-      $("#show-number").html("<h2>" + number + "</h2>");
+        //  Show the number in the #show-number tag.
+        $("#show-number").html("<h2>" + number + "</h2>");
 
 
-      //  Once number hits zero...
-      if (number === 0) {
+        //  Once number hits zero...
+        if (number === 0) {
 
-        //  ...run the stop function.
-        // stop();
+            //  ...run the stop function.
+            stop();
 
-        //  Alert the user that time is up.
-        // alert("Time Up!");
-      }
+            //  Alert the user that time is up.
+            alert("Time Up!");
+        }
     }
 
     run();
 
-    
+    function stop() {
+        intervalId = clearInterval(intervalId);
+
+        //game over screen display scores
+        if (intervalId === 0) {
+
+        }
+    }
+
+});
+
+$( "input" ).on( "click", function() {
+  $( "#log10" ).html( $( "#questionTenContainer input:checked" ).val());
+
+});
+
+
 //game times out if timer is less than 0
 //switches to game over screen with correct answers and incorrect answers and unanswered
 
-// function clock {
-// 	timer = setInterval()
-
-// }
-
-//array of questions and answers
 
 //key value pair down there how to use
 
-let myArr = {
-	'Is the sky blue?': ['yes','no','pizza']
+// let myArr = {
+// 	'Is the sky blue?': ['yes','no','pizza']
 
-}
+// }
 
 
-var questionArray = {
-  q1: 'What?',
-  answers1: ["#button1a", "touched tree", "pizza", "running"],
-  q2: 'Twat?',
-  answers2: ['twerk', "teepee", "cheezus", "walking"]
+// var questionArray = {
+//   q1: 'What?',
+//   answers1: ["#button1a", "touched tree", "pizza", "running"],
+//   q2: 'Twat?',
+//   answers2: ['twerk', "teepee", "cheezus", "walking"]
 
-}
+// }
 
+// if (#q1 == 2) {
+// 	wins++;
+
+// 	console.log(wins);
+// }
